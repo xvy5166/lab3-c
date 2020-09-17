e971de0d1d5b6c7846ad88a5f791a8d6afdc6c42
XinyiY5166 <xvy5166@psu.edu>
2020-09-17 14:32:11 +0000

    -.

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ void print_n(const char *s, int n){
 int main(void) {
   int i = atof(readline("Enter an int: "));
   int number = sum_n(i);
-  printf("sum is %d.\n",number);
+  printf("sum is %d\n",number);
   char *string = readline("Enter a string: ");
   print_n(string, i);
   return 0;
